7948daff3b0ab411fe1b499d8dbc081ff7b08b49
Lucas Vieira <lucasvieira@protonmail.com>
2018-04-01 11:14:10 -0300

    add more functions to orc-battle (ch. 9, p. 179)

--- orc-battle.lisp
+++ orc-battle.lisp
@@ -60,16 +60,16 @@
   (fresh-line)
   (princ "Attack style: [s]tab  [d]ouble swing  [r]oundhouse: ")
   (case (read)
-    (s (monster-hit (pick-monster)
+    (s (monster-hit (pick-monster) ; stab
                     (+ 2 (randval (ash *player-strength* -1)))))
     (d (let ((x (randval (truncate (/ *player-strength* 6)))))
-         (princ "Your double swing has a strength of ")
+         (princ "Your double swing has a strength of ") ; double swing
          (princ x)
          (fresh-line)
          (monster-hit (pick-monster) x)
          (unless (monsters-dead)
            (monster-hit (pick-monster) x))))
-    (otherwise
+    (otherwise ; roundhouse
      (dotimes (x (1+ (randval (truncate (/ *player-strength* 3)))))
        (unless (monsters-dead)
          (monster-hit (random-monster 1)))))))
@@ -77,4 +77,66 @@
 (defun randval (n)
   "Retrieves an inclusive random value between 1 and n."
   (1+ (random (max 1 n))))
+
+
+(defun random-monster ()
+  "Picks up a random monster that isn't already dead."
+  (let ((m (aref *monsters* (random (length *monsters*)))))
+    (if (monster-dead m)
+        (random-monster)
+        m)))
+
+
+(defun pick-monster ()
+  "Picks up a nonrandom monster."
+  (fresh-line)
+  (princ "Monster #: ")
+  (let ((x (read)))
+    (if (not (and (integerp x)
+              (>= x 1)
+              (<= x *monster-num*)))
+        (progn (princ "That is not a valid monster number.")
+               (pick-monster))
+        (let ((m (aref *monsters* 1- x)))
+          (if (monster-dead m)
+              (progn (princ "That monster is already dead.")
+                     (pick-monster))
+              m)))))
+
+
+
+;; Monster management
+
+(defun init-monsters ()
+  (setf *monsters*
+        (map 'vector
+             (lambda (x)
+               (funcall (nth (random (length *monster-builders*))
+                             *monster-builders*)))
+             (make-array *monster-num*))))
+
+(defun monster-dead (m)
+  (<= (monster-health m) 0))
+
+(defun monsters-dead ()
+  (every #'monster-dead *monsters*))
+
+(defun show-monsters ()
+  (fresh-line)
+  (princ "Your foes: ")
+  (let ((x 0))
+    (map 'list
+         (lambda (m)
+           (fresh-line)
+           (princ "  ")
+           (princ (incf x))
+           (princ ", ")
+           (if (monster-dead m)
+               (princ "**dead**")
+               (progn (princ "(Health=")
+                      (princ (monster-health m))
+                      (princ ") ")
+                      (monster-show m))))
+         *monsters*)))
+
 
